c27a77db8cbec46e7e643d5ecc9a32420d96d42d
Pnghikiet <phungnghikiet@gmail.com>
2025-04-18 17:32:58 +0700

    add todo featrue

--- ToDoListApplication/Controllers/ToDosController.cs
+++ ToDoListApplication/Controllers/ToDosController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ToDoListApplication.DataAccess.Data;
+using ToDoListApplication.DataAccess.Models;
 
 namespace ToDoListApplication.Controllers
 {
@@ -15,5 +17,37 @@ namespace ToDoListApplication.Controllers
             _db = db;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<List<ToDoItem>>> GetAllItem()
+        {
+            var todos = await _db.ToDoItems.ToListAsync();
+
+            return Ok(todos);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> CreateToDo(ToDoItem todo)
+        {
+            var todoToCreate = todo;
+
+            await _db.ToDoItems.AddAsync(todoToCreate);
+
+            return Ok(todoToCreate);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ToDoItem>> DeleteTodo(int id)
+        {
+            var todo = await _db.ToDoItems.FindAsync(id);
+
+            if (todo == null)
+                return NotFound();
+
+            _db.ToDoItems.Remove(todo);
+            await _db.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
